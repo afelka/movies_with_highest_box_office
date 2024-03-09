7b25a7721b9ee43a23a00dce8e2dfcc8a65ad7ec
afelka <erdememin@gmail.com>
2024-03-10 00:30:01 +0100

    correct csv name, change the ggplot

--- movies_with_highest_box_office_ggplot.R
+++ movies_with_highest_box_office_ggplot.R
@@ -3,29 +3,28 @@ library(ggimage)
 library(dplyr)
 library(ggrepel)
 
-
-movies_with_highest_box_office <- read.csv("movies_with_highest_box_office.csv.csv")
+movies_with_highest_box_office <- read.csv("movies_with_highest_box_office.csv")
 
 movies_with_highest_box_office_top_250 <- movies_with_highest_box_office %>% arrange(desc(worldwide_gross)) %>%
   head(250)
 
-highest_avg_rating <- movies_with_highest_box_office_top_250 %>% arrange(desc(avg_rating)) %>% slice(1)
-lowest_avg_rating <- movies_with_highest_box_office_top_250 %>% arrange(avg_rating) %>% slice(1)
+highest_avg_rating <- movies_with_highest_box_office_top_250 %>% arrange(desc(rating)) %>% slice(1)
+lowest_avg_rating <- movies_with_highest_box_office_top_250 %>% arrange(rating) %>% slice(1)
 
 # Create a ggplot2 plot with book covers as points
-gg <- ggplot(movies_with_highest_box_office, aes(x = avg_rating, y = no_of_ratings)) +
-  geom_image(aes(image = img_src), size = 0.03) +  # Add book covers
-  geom_text_repel(data = highest_avg_rating, aes(x = avg_rating, y = no_of_ratings, 
-                                                 label = paste0(substr(book_names,1,24), "\nhas the highest avg.(",avg_rating ,
-                                                                ")\nrating across most rated books \n")),
-                  color = "orange", size = 2 , vjust = -0.25, hjust = 0.7) +
-  geom_text_repel(data = lowest_avg_rating, aes(x = avg_rating, y = no_of_ratings, 
-                                                label = paste0(book_names, "\nhas the lowest avg.(",avg_rating ,
-                                                               ")\nrating across most rated books \n")),
-                  color = "purple", size = 2 , vjust = -2.25, hjust = 0.7) +
-  labs(title = "Box Office vs Average Rating for the Movies with Highest Box Office Revenue",
-       x = "Average Rating",
-       y = "Office") +
+gg <- ggplot(movies_with_highest_box_office_top_250, aes(x = rating, y = worldwide_gross)) +
+  geom_image(aes(image = image_name), size = 0.03) +  # Add book covers
+  geom_text_repel(data = highest_avg_rating, aes(x = rating, y = worldwide_gross, 
+                                                 label = paste0(movie_title, "\nhas the highest avg.(",rating ,
+                                                                ")\nrating across movies with highest box office \n")),
+                  color = "orange", size = 1.8 , vjust = -0.25, hjust = 0.7) +
+  geom_text_repel(data = lowest_avg_rating, aes(x = rating, y = worldwide_gross, 
+                                                label = paste0(movie_title, "\nhas the lowest avg.(",rating ,
+                                                               ")\nrating across movies with highest box office \n")),
+                  color = "purple", size = 2 , vjust = -1.25, hjust = 0.7) +
+  labs(title = "Box Office vs Average IMDB Rating for the Movies with Highest Box Office Revenue",
+       x = "Average IMDB Rating",
+       y = "Box Office in $") +
   scale_y_continuous(labels = scales::comma) +  
   theme_minimal()  
 
