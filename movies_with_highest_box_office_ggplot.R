b17b1e970b9ca27eeb3afe188ecf3d66c45e5f32
afelka <erdememin@gmail.com>
2024-03-10 17:21:38 +0100

    create stacked plot with movie posters

--- movies_with_highest_box_office_ggplot.R
+++ movies_with_highest_box_office_ggplot.R
@@ -35,3 +35,34 @@ gg <- ggplot(movies_with_highest_box_office_top_250, aes(x = rating, y = worldwi
 
 # Save the plot as an image file
 ggsave( "movies_with_highest_box_office.png",plot = gg, bg="white")
+
+movies_with_highest_box_office_top_250_grouped <- movies_with_highest_box_office_top_250 %>% group_by(year) %>%
+                                                  mutate(no_of_movies = n()) %>% ungroup()
+
+# create a plot with movie posters stacked on top of each other on year
+gg2 <- ggplot(movies_with_highest_box_office_top_250_grouped, aes(x = year, 1)) + 
+  geom_image(aes(image=image_name), size=.03, position = "stack") +
+  geom_point(aes(x= year , y = no_of_movies), color = "white") +
+  geom_text(aes(y = no_of_movies,label = no_of_movies),
+            vjust = -4,
+            colour = "#143c8a",
+            size = 3.5) +
+  theme_classic() +
+  scale_y_continuous(
+    limits = c(0, round(
+      max(movies_with_highest_box_office_top_250_grouped$no_of_movies) + 3),
+      digits = (1 - nchar(
+        max(movies_with_highest_box_office_top_250_grouped$no_of_movies)
+      ))
+    )) +
+  scale_x_continuous(breaks = seq(min(movies_with_highest_box_office_top_250_grouped$year), 
+                                  max(movies_with_highest_box_office_top_250_grouped$year), 
+                                  by = 1)) +
+  labs(title = "Number of Movies in top 250 Box Office List by Year") +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(angle = 45, vjust = 0.5)
+  ) 
+
+# Save the plot as an image file
+ggsave( "movies_with_highest_box_office_by_year.png",plot = gg2, bg="white")
